53aa5bc9618e6ed5c211bd1f1015c3c204d6d32d
Emily Wells Jimenez and Kyle Ebersole <kyle@>
2017-09-27 09:16:09 -0700

    initial file setup and layout design.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//Business Logic Here
+var dice = {
+  numbers: 6,
+  roll: function () {
+    var random = Math.floor(Math.random() * this.numbers) + 1;
+    return random;
+  }
+}
+
+//UI Logic Here
+function 
